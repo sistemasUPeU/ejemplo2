bf30a8ec13e95240e2c39b4d9237fc05af1aa6d2
chicho <alphateam.sistemas@upeu.edu.pe>
2019-10-22 15:09:07 -0500

    este es ujemplo

---
+++ src/main/java/com/ejemplo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ejemplo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prueba")
+public class HomeController {
+
+	
+	@GetMapping
+	public String get() {
+		return "esto es una prueba";
+	}
+}
